10261a543ccc1d3244c4f79d87ff822f650ce290
MARomain <33660355+MARomain@users.noreply.github.com>
2019-10-05 15:47:24 +0200

    Unity demande un backup aled

---
+++ Unity Project/Assets/Scripts/FlickeryLight.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlickeryLight : MonoBehaviour
+{
+    public Light light;
+    public float multiplier = 2f;
+
+    private void Start()
+    {
+        light = GetComponent<Light>();
+    }
+
+    void Update()
+    {
+        light.intensity = Mathf.Sin(Time.time *multiplier) +1.5f;
+    }
+}
