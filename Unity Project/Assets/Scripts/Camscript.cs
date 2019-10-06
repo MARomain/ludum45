97538aad05a0e23508148650da121ab1ea36c324
MARomain <33660355+MARomain@users.noreply.github.com>
2019-10-06 16:44:24 +0200

    En faites j'ai oublié d'importer les trucs, folle

---
+++ Unity Project/Assets/Scripts/Camscript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camscript : MonoBehaviour
+{
+    public Camera cam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+
+        float[] distances = new float[32];
+        distances[0] = 10f; //smaller than your camera clipping planes's far value
+        Camera.main.layerCullDistances = distances;
+        Camera.main.layerCullSpherical = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(cam.layerCullSpherical);
+
+    }
+}
